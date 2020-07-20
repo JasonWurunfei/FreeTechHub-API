37f82851ed8648e2833fcf2c54037c9c73d74baa
YKK-cp <1347256906@qq.com>
2020-07-20 16:25:09 +0800

    add Question model

--- question/urls.py
+++ question/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from rest_framework import routers
+from .views import QuestionViewSet
+
+router = routers.DefaultRouter()
+router.register('question', QuestionViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
--- question/views.py
+++ question/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import QuestionSerializer
+from .models import Question
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [
+        IsAuthenticated,
+        IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        print(type(request.data))
+        data = {
+            'csrfmiddlewaretoken': request.data['csrfmiddlewaretoken'],
+            'title': request.data['title'],
+            'content': request.data['content'],
+            'rewarded_money':request.data['rewarded_money'],
+            'viewTimes': 0,
+            'note': request.data['note'],
+            'status': False,
+            'question_type': True,
+            'owner' : request.user.id
+        }
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
