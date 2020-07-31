7be6bf1c1420ef6c851d7902bfd80de978132e29
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2020-07-31 19:53:33 +0800

    add UpdateSelectedView in blog app to refactory the editing and creating of series

--- blog/views.py
+++ blog/views.py
@@ -58,3 +58,50 @@ class QueryView(APIView):
         user_related_content['blog'] = [BlogSerializer(blog).data for blog in blogs]
         user_related_content['series'] = [SeriesSerializer(series).data for series in all_series]
         return Response(user_related_content)
+
+
+class UpdateSelectedView(APIView):
+    """
+    This view will update the foreign key of sub series 
+    or related blogs of one series
+    """
+    def post(self, request, *args, **kwargs):
+        
+        series = Series.objects.get(id=request.data['series'])
+        selected_blogs = request.data['selected_items']['blog']
+        selected_series = request.data['selected_items']['series']
+
+        # if it recursively selected itself, remove it.
+        if series.id in selected_series:
+            selected_series.remove(series.id)
+
+        # If related_blogs and related_series is not empty, it means
+        # that this view is called when editing an existing series
+        
+        # remove not selected blogs but used to be in that series
+        related_blogs = Blog.objects.filter(series=series)
+        for blog in related_blogs:
+            if blog.id not in selected_blogs:
+                blog.series = None          # point the foreign key to null to remove
+                blog.save()
+        
+        # remove not selected series but used to be in that series
+        related_series = Series.objects.filter(sub_series_of=series)
+        for sub_series in related_series:
+            if sub_series.id not in selected_series:
+                sub_series.sub_series_of = None # point the foreign key to null to remove
+                sub_series.save()
+
+        # add selected blogs
+        for blog_id in selected_blogs:
+            blog = Blog.objects.get(id=blog_id)
+            blog.series = series
+            blog.save()
+
+        # add selected series
+        for series_id in selected_series:
+            sub_series = Series.objects.get(id=series_id)
+            sub_series.sub_series_of = series
+            sub_series.save()
+
+        return Response(SeriesSerializer(series).data)
