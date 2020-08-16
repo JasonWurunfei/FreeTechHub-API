e11ea8da1d084d9e3385c80e7373f9b486051575
YKK-cp <1347256906@qq.com>
2020-08-16 15:58:42 +0800

    update comment model

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Comment
+from user.serializers import UserSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
+    owner_instance = UserSerializer(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ["id", "content", "time", "owner","sub_comments_of","sub_comments"]
+        fields = ["id", "content", "time", "owner", "owner_instance", "sub_comments_of","sub_comments"]
         extra_kwargs = {
             'sub_comments': {'read_only': True}
         }
