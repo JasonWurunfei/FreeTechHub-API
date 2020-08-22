b71ac09257ea453d1f6f4d688365ffcb7368af5e
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2020-08-22 13:31:01 +0800

    remove debug prints

--- skilltree/views.py
+++ skilltree/views.py
@@ -22,9 +22,7 @@ class SkilltreeView(APIView):
     def post(self, request, tree_id, format=None):
         tree_handler = SkillTreeHandler(tree_id)
         modify_queue = request.data['modify_queue']
-        print(modify_queue)
         tree_handler.read_modify(modify_queue)
-        print(tree_handler.modify_queue)
         tree_handler.commit()
         return Response(tree_handler.get_tree(), status.HTTP_202_ACCEPTED)
 
