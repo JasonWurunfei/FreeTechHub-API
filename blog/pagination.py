7876eeabe4b8bac0cc26244f8983956f10bcf8f5
YKK-cp <1347256906@qq.com>
2020-10-08 14:27:19 +0800

    add pagination const in settings

--- blog/pagination.py
+++ blog/pagination.py
@@ -1,6 +1,11 @@
 from rest_framework import pagination
+from django.conf import settings
 
 class Pagination(pagination.PageNumberPagination):
+<<<<<<< Updated upstream
     page_size = 5
+=======
+    page_size = settings.PAGE_SIZE
+>>>>>>> Stashed changes
     page_query_param = 'page'
     page_size_query_param = 'num'
